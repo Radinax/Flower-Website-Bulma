c79b336a1033a399e55bcaac061032e7cee36cf1
Adrian Beria <adrberia@gmail.com>
2018-03-10 17:54:49 -0400

    Initial Commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "globals": {
+        "window": true,
+        "document": true
+    },
+    "rules": {
+        "no-console": "off",
+        "no-plusplus": "off",
+        "no-default-case": "off",
+        "no-loop-func": "off",
+        "no-unused-expressions": "off"
+    }
+};
